a5210119814910a5a6acf14959b8d920d322a20b
Roman Ojha <razzroman98@gmail.com>
2022-03-17 19:07:23 +0545

    initialized truffle project

---
+++ Class/Class/05_Initialized_a_Project_in_Truffle/05_Initialized_a_Project_in_Truffle.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.6.0 <0.9.0;
+
+/*
+ *) Initialized In VScode
+    -> go to project directory:
+      -> truffle init (to initilaized truffle project)
+   -> Folder directory:
+      -> contracts (contract contain all the smart contract that we will create)
+      -> migrations (when we will deploy contract at that time we will use migration)
+      -> test (for testing code)
+      -> truffle-config.js (all the configrution for truffle)
+ */
