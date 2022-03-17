630edbee31f393b0dafce9a2e54884dc6e5942e9
Roman Ojha <razzroman98@gmail.com>
2022-03-17 18:09:43 +0545

    truffle course, Prerequisites

---
+++ Class/01_Truffle_Curse_Overview/01_Truffle_Curse_Overview.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.7.0 <0.9.0;
+
+/*
+ *) Course Overview:
+    -> Ganache
+    -> Writing Contract in VS code using Truffle
+    -> Truffle Compilation
+    -> Truffle Migration
+    -> Infura
+    -> Deploy contract on testnet
+    -> Frontend Intergration
+*/
