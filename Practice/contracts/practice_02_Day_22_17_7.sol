85718b5d1c99c83d4abc3787415c8fde04742af4
Roman-Ojha <razzroman98@gmail.com>
2022-07-17 14:31:42 +0545

    fix: added migrations contract for migrate

---
+++ Practice/contracts/practice_02_Day_22_17_7.sol
@@ -0,0 +1,5 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.6.0 <0.9.0;
+
+contract Practice02 {}
--- Practice/migrations/2_practice.js
+++ Practice/migrations/2_practice.js
@@ -1,5 +1,7 @@
 const Practice01Demo = artifacts.require("Practice01Demo");
+const Practice02 = artifacts.require("Practice02");
 
 module.exports = function (deployer) {
   deployer.deploy(Practice01Demo);
+  deployer.deploy(Practice02);
 };
